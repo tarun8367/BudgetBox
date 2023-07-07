7f46f328a0ffd07d0e5532d50fed3981b235ff8d
Tarun Kumar <tarunkumar8367@gmail.com>
2023-07-07 23:34:30 +0530

    Pagination Done

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -8,9 +8,11 @@ export function fetchAllProducts() {
   );
 }
 
-export function fetchProductsByFilters(filter) {
+export function fetchProductsByFilters(filter,sort,pagination) {
   // filter = {"category":["smartphone","laptops"]}
     // sort = {_sort:"price" , _order ="desc"}
+
+    // pagination = {_page : 1 , _limit : 10}
     // TODO : on server it will support multiple categories
   let queryString = '';
   for(let key in filter){
@@ -21,12 +23,21 @@ export function fetchProductsByFilters(filter) {
     }
   }
 
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`;
+  }
+
+  for(let key in pagination){
+    queryString += `${key}=${pagination[key]}&`;
+  }
+  
+
 
   return new Promise(async (resolve) =>{
     //TODO: we will not hard-code server URL here
     const response = await fetch('http://localhost:8080/products?'+queryString) 
     const data = await response.json()
-    resolve({data})
-  }
+    const totalItems = await response.headers.get('X-Total-Count')
+    resolve({data: {products: data, totalItems:+totalItems}})  }
   );
 }

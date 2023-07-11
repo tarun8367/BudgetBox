fc416a93947dce4ac785efd99c8785bd13ec212b
Tarun Kumar <tarunkumar8367@gmail.com>
2023-07-11 23:33:21 +0530

    UI Updated

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Navbar from '../features/navbar/Navbar'
-import ProductDetail from '../features/product/components/ProductDetail'
+import NavBar from "../features/navbar/Navbar";
+import ProductDetail from "../features/product/components/ProductDetail";
+import Footer from "../features/common/Footer";
 
 function ProductDetailPage() {
-  return (
-    <div>
-      <Navbar />
-      <ProductDetail />
-    </div>
-  )
+    return ( 
+        <div>
+            <NavBar>
+                <ProductDetail></ProductDetail>
+            </NavBar>
+            <Footer></Footer>
+        </div>
+     );
 }
 
-export default ProductDetailPage
+export default ProductDetailPage;
